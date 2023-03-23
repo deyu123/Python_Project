22f416b7d49cf09589a0928a64551a8a9c9d9d22
deyu <deyu.zhang@tendcloud.com>
2023-03-24 00:10:07 +0800

    2023-03-24 1.test push

--- com/deyu/openai/printAIImage.py
+++ com/deyu/openai/printAIImage.py
@@ -3,7 +3,6 @@ if __name__ == '__main__':
 
     # 设置 OpenAI API 密钥
     openai.api_key = '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'
-
     response = openai.Image.create(
       prompt="a white siamese cat",
       n=1,
